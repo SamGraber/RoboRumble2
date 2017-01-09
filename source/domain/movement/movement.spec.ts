1c203588b36e2768ee9958509b21aeb4dccd197c
SamGraber <authorsamgraber@gmail.com>
2017-01-09 17:30:13 -0500

    Moved the heavy lifting of movement to the movement engine
    The movement engine's main responsibility is to handle movement, so it makes more sense to have the heavy movement logic here. The map is now no more than a simple container for all the map items.

--- source/domain/movement/movement.spec.ts
+++ source/domain/movement/movement.spec.ts
@@ -1,41 +1,215 @@
 import { MovementEngine, Rotation } from './movement';
-import { heading } from '../../utilities/angles/index';
+import { ConcreteBlock } from '../boardElements/concreteBlock';
+import { Point, Vector, Ray, heading } from '../../utilities/angles/index';
+import { Robot } from '../robot/index';
+import { Map } from '../map/map';
 
 interface MapMock {
 	move: Sinon.SinonSpy;
 }
 
 describe('MovementEngine', () => {
-	let movementEngine: MovementEngine;
-	let map: MapMock;
+	describe('abstraction layer', () => {
+		let movementEngine: MovementEngine;
 
-	beforeEach(() => {
-		map = { move: sinon.spy() };
-		movementEngine = new MovementEngine(<any>map);
+		beforeEach(() => {
+			movementEngine = new MovementEngine(<any>{});
+		});
+
+		it('should tell the map to move the item', () => {
+			const item = { heading: '123' };
+			const moveSpy = sinon.spy();
+			movementEngine.executeMove = moveSpy;
+
+			movementEngine.move(<any>item);
+
+			sinon.assert.calledOnce(moveSpy);
+			sinon.assert.calledWith(moveSpy, item, item.heading);
+		});
+
+		it('should change the heading to the next clockwise orientation', () => {
+			const item = { heading: heading.north };
+
+			movementEngine.turn(<any>item, Rotation.clockwise);
+
+			expect(item.heading.equals(heading.east)).to.be.true;
+		});
+
+		it('should change the heading to the next counter-clockwise orientation', () => {
+			const item = { heading: heading.north };
+
+			movementEngine.turn(<any>item, Rotation.counterClockwise);
+
+			expect(item.heading.equals(heading.west)).to.be.true;
+		});
 	});
 
-	it('should tell the map to move the item', () => {
-		const item = { heading: '123' };
-
-		movementEngine.move(<any>item);
-
-		sinon.assert.calledOnce(map.move);
-		sinon.assert.calledWith(map.move, item, item.heading);
-	});
-
-	it('should change the heading to the next clockwise orientation', () => {
-		const item = { heading: heading.north };
-
-		movementEngine.turn(<any>item, Rotation.clockwise);
-
-		expect(item.heading.equals(heading.east)).to.be.true;
-	});
-
-	it('should change the heading to the next counter-clockwise orientation', () => {
-		const item = { heading: heading.north };
-
-		movementEngine.turn(<any>item, Rotation.counterClockwise);
-
-		expect(item.heading.equals(heading.west)).to.be.true;
+	describe('implementation layer', () => {
+		it('should collide with wall', () => {
+			const floor = new ConcreteBlock();
+			floor.coordinate = new Point(0, 0, 0);
+			floor.size = { x: 2, y: 2, z: 0 };
+			
+			const wall = new ConcreteBlock();
+			wall.coordinate = new Point(1, 0, 0);
+			wall.size = { x: 0, y: 1, z: 1 };
+			
+			const robot = new Robot();
+			robot.coordinate = new Point(0, 0, 0);
+			robot.heading = heading.east;
+			
+			const map = new Map();
+			map.items.push(floor);
+			map.items.push(wall);
+			map.items.push(robot);
+			
+			map.movementEngine.executeMove(robot, robot.heading);
+			
+			expect(robot.coordinate.x).to.equal(0);
+			expect(robot.coordinate.y).to.equal(0);
+			expect(robot.coordinate.z).to.equal(0);
+		});
+		
+		it('should push a robot', () => {
+			const floor = new ConcreteBlock();
+			floor.coordinate = new Point(0, 0, 0);
+			floor.size = { x: 3, y: 3, z: 0 };
+			
+			const robot = new Robot();
+			robot.coordinate = new Point(0, 0, 0);
+			robot.heading = heading.east;
+			
+			const otherRobot = new Robot();
+			otherRobot.coordinate = new Point(1, 0, 0);
+			otherRobot.heading = heading.south; //heading should be irrelevant when being pushed
+			
+			const map = new Map();
+			map.items.push(floor);
+			map.items.push(robot);
+			map.items.push(otherRobot);
+			
+			map.movementEngine.executeMove(robot, robot.heading);
+			
+			expect(robot.coordinate.x).to.equal(1);
+			expect(robot.coordinate.y).to.equal(0);
+			expect(robot.coordinate.z).to.equal(0);
+			expect(otherRobot.coordinate.x).to.equal(2);
+			expect(otherRobot.coordinate.y).to.equal(0);
+			expect(otherRobot.coordinate.z).to.equal(0);
+		});
+		
+		it('should not push two robots', () => {
+			const floor = new ConcreteBlock();
+			floor.coordinate = new Point(0, 0, 0);
+			floor.size = { x: 4, y: 4, z: 0 };
+			
+			const robot = new Robot();
+			robot.coordinate = new Point(0, 0, 0);
+			robot.heading = heading.east;
+			
+			const robot2 = new Robot();
+			robot2.coordinate = new Point(1, 0, 0);
+			robot2.heading = heading.south; //heading should be irrelevant when being pushed
+			
+			const robot3 = new Robot();
+			robot3.coordinate = new Point(2, 0, 0);
+			robot3.heading = heading.north; //heading should be irrelevant when being pushed
+			
+			const map = new Map();
+			map.items.push(floor);
+			map.items.push(robot);
+			map.items.push(robot2);
+			map.items.push(robot3);
+			
+			const move = map.movementEngine.executeMove(robot, robot.heading);
+			
+			expect(move).to.equal(false);
+			expect(robot.coordinate.x).to.equal(0);
+			expect(robot.coordinate.y).to.equal(0);
+			expect(robot.coordinate.z).to.equal(0);
+			expect(robot2.coordinate.x).to.equal(1);
+			expect(robot2.coordinate.y).to.equal(0);
+			expect(robot2.coordinate.z).to.equal(0);
+			expect(robot3.coordinate.x).to.equal(2);
+			expect(robot3.coordinate.y).to.equal(0);
+			expect(robot3.coordinate.z).to.equal(0);
+		});
+		
+		it('should fall off the edge of the map', () => {
+			const floor = new ConcreteBlock();
+			floor.coordinate = new Point(0, 0, 0);
+			floor.size = { x: 1, y: 1, z: 0 };
+			
+			const robot = new Robot();
+			robot.coordinate = new Point(0, 0, 0);
+			robot.heading = heading.east;
+			
+			const map = new Map();
+			map.items.push(floor);
+			map.items.push(robot);
+			
+			map.movementEngine.executeMove(robot, robot.heading);
+			
+			expect(map.items).to.not.contain(robot);
+		});
+		
+		it('should fall to a lower level', () => {
+			const floor = new ConcreteBlock();
+			floor.coordinate = new Point(0, 0, 0);
+			floor.size = { x: 2, y: 2, z: 0 };
+			
+			const platform = new ConcreteBlock();
+			platform.coordinate = new Point(0, 0, 0);
+			platform.size = { x: 1, y: 1, z: 1 };
+			
+			const robot = new Robot();
+			robot.coordinate = new Point(0, 0, 1); // on top of the platform
+			robot.heading = heading.east;
+			
+			const map = new Map();
+			map.items.push(floor);
+			map.items.push(platform);
+			map.items.push(robot);
+			
+			map.movementEngine.executeMove(robot, robot.heading);
+			
+			expect(robot.coordinate.x).to.equal(1);
+			expect(robot.coordinate.y).to.equal(0);
+			expect(robot.coordinate.z).to.equal(0);
+		});
+		
+		it('should not intersect with cube below', () => {
+			const vector = new Vector();
+			vector.origin = new Point(0.5, 0.5, 0.5);
+			vector.offset = new Point(1.5, 0.5, 0.5);
+			const ray = new Ray(vector);
+			const cube = {
+				coordinate: new Point(0, 0, 0),
+				size: { x: 2, y: 2, z: 0 },
+			};
+			
+			const map = new Map();
+			
+			const collision = map.movementEngine.intersect(ray, <any>cube);
+			
+			expect(collision).to.equal(false);
+		});
+		
+		it('should intersect with wall', () => {
+			const vector = new Vector();
+			vector.origin = new Point(0.5, 0.5, 0.5);
+			vector.offset = new Point(1.5, 0.5, 0.5);
+			const ray = new Ray(vector);
+			const cube = {
+				coordinate: new Point(1, 0, 0),
+				size: { x: 0, y: 1, z: 1 },
+			};
+			
+			const map = new Map();
+			
+			const collision = map.movementEngine.intersect(ray, <any>cube);
+			
+			expect(collision).to.equal(true);
+		});
 	});
 });
--- source/domain/movement/movement.ts
+++ source/domain/movement/movement.ts
@@ -1,6 +1,8 @@
+import { each, remove } from 'lodash';
+
 import { Map } from '../map/map';
-import { IMoveableMapItem } from '../map/mapItem/index';
-import { heading } from '../../utilities/angles/index';
+import { IMapItem, IMoveableMapItem, Permeability } from '../map/mapItem/index';
+import { Point, Ray, heading } from '../../utilities/angles/index';
 
 export enum Rotation {
 	clockwise = 0,
@@ -11,8 +13,8 @@ export enum Rotation {
 export class MovementEngine {
 	constructor(public map: Map) {}
 
-	move(item: IMoveableMapItem): void {
-		this.map.move(item, item.heading);
+	move(item: IMoveableMapItem, heading?: Point): void {
+		this.executeMove(item, heading || item.heading);
 	}
 
 	turn(item: IMoveableMapItem, rotation: Rotation): void {
@@ -20,6 +22,128 @@ export class MovementEngine {
 			item.heading = heading.clockwise(item.heading);
 		} else if (rotation === Rotation.counterClockwise) {
 			item.heading = heading.counterClockwise(item.heading);
+		}
+	}
+
+	executeMove(movingItem: IMapItem, direction: Point, pushed?: boolean): boolean {
+		const map = this.map;
+		const movementEngine = this;
+		let stop;
+		
+		//add (0.5, 0.5, 0.5) to the coordinate of the moving item to measure from the center of the space
+		const origin = movingItem.coordinate.add(new Point(0.5, 0.5, 0.5));
+		const movementRay = new Ray(origin.toVector(direction));
+		
+		//apply some filter to the map items
+		each(map.items, item => {
+			if (checkForCollision(item, movementRay)) {
+				stop = true;
+			}
+		});
+		
+		if (!stop) {
+			movingItem.coordinate = movingItem.coordinate.add(direction);
+		}
+
+		gravity();
+		return !stop;
+		
+		function checkForCollision(item: IMapItem, ray: Ray) {
+			let stop;
+			if (item === movingItem) {
+				//item can't collide with itself
+				return;
+			}
+			
+			if (movementEngine.intersect(ray, item)) {
+				if (item.permeability === Permeability.nonpermeable) {
+					stop = true;
+				}
+				else if (item.permeability === Permeability.moveable) {
+					//only one item can be pushed. If the moving item was pushed, moveable collisions are treated the same as nonpermeable
+					if (!pushed) {
+						if (movementEngine.executeMove(item, direction, true) === false) {
+							stop = true;
+						}
+					}
+					else {
+						stop = true;
+					}
+				}
+				//call interaction functions
+			}
+			return stop;
+		}
+		
+		function gravity() {
+			let stop = false;
+			
+			//add (0.5, 0.5, 0.5) to the coordinate of the moving item to measure from the center of the space
+			const origin = movingItem.coordinate.add(new Point(0.5, 0.5, 0.5));
+			const movementRay = new Ray(origin.toVector(heading.down));
+			
+			//apply some filter to the map items
+			each(map.items, item => {
+				if (checkForCollision(item, movementRay)) {
+					stop = true;
+				}
+			});
+
+			if (!stop) {
+				movingItem.coordinate = movingItem.coordinate.add(heading.down);
+				if (movingItem.coordinate.z < map.bottom) {
+					//item falls off the map
+					remove(map.items, movingItem);
+					return;
+				}
+				gravity();
+			}
+		}
+	}
+
+	intersect(ray: Ray, cube: IMapItem): boolean {
+		const bounds = [ 
+			cube.coordinate, 
+			new Point(cube.coordinate.x + cube.size.x, 
+			cube.coordinate.y + cube.size.y, 
+			cube.coordinate.z + cube.size.z), 
+		];
+
+		let minTimeToIntersect, maxTimeToIntersect, minTimeToYIntersect, maxTimeToYIntersect, minTimeToZIntersect, maxTimeToZIntersect;
+		minTimeToIntersect = (bounds[ray.sign[0]].x - ray.origin.x) * ray.inverseOffset.x;
+		maxTimeToIntersect = (bounds[1 - ray.sign[0]].x - ray.origin.x) * ray.inverseOffset.x;
+		minTimeToYIntersect = (bounds[ray.sign[1]].y - ray.origin.y) * ray.inverseOffset.y;
+		maxTimeToYIntersect = (bounds[1 - ray.sign[1]].y - ray.origin.y) * ray.inverseOffset.y;
+		
+		if ((minTimeToIntersect > maxTimeToYIntersect) || (minTimeToYIntersect > maxTimeToIntersect)) {
+			return false;
+		}
+		if (minTimeToYIntersect > minTimeToIntersect) {
+			minTimeToIntersect = minTimeToYIntersect;
+		}
+		if (maxTimeToYIntersect < maxTimeToIntersect) {
+			maxTimeToIntersect = maxTimeToYIntersect;
+		}
+
+		minTimeToZIntersect = (bounds[ray.sign[2]].z - ray.origin.z) * ray.inverseOffset.z;
+		maxTimeToZIntersect = (bounds[1 - ray.sign[2]].z - ray.origin.z) * ray.inverseOffset.z;
+
+		if ((minTimeToIntersect > maxTimeToZIntersect) || (minTimeToZIntersect > maxTimeToIntersect)) {
+			return false;
+		}
+		if (minTimeToZIntersect > minTimeToIntersect) {
+			minTimeToIntersect = minTimeToZIntersect;
+		}
+		if (maxTimeToZIntersect < maxTimeToIntersect) {
+			maxTimeToIntersect = maxTimeToZIntersect;
+		}
+		
+		//a value of < 0 or > 1 indicates that the collision happens outside of the length of the Ray
+		if ((minTimeToIntersect > 0 && minTimeToIntersect < 1) || (maxTimeToIntersect > 0 && maxTimeToIntersect < 1)) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 }
