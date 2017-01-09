b98ea27064832af541e72124ec0678751264eeff
SamGraber <authorsamgraber@gmail.com>
2017-01-09 14:42:24 -0500

    Setup the system config to be able to handle node modulse with ease

--- system.config.js
+++ system.config.js
@@ -1,16 +1,32 @@
 var map = {};
 
+var nodeModules = [
+	'lodash',
+];
+
+var defaultPackages = [
+	'lodash',
+];
+
 var packages = {
 	'source': {
 		defaultExtension: 'js',
-	},
-	'lodash': {
-		main: 'index.js',
 	},
 	'node_modules': {
 		defaultExtension: 'js',
 	},
 };
+
+function setupNodeModule(module) {
+	map[module] = `node_modules/${module}`;
+}
+
+function setupDefaultPackage(package) {
+	packages[package] = { main: 'index.js' };
+}
+
+nodeModules.forEach(setupNodeModule);
+defaultPackages.forEach(setupDefaultPackage);
 
 System.config({
 	map: map,
