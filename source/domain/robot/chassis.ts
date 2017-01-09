a929558592f181d01c14fcbad6e2e3d19cb619b6
SamGraber <authorsamgraber@gmail.com>
2017-01-09 15:49:13 -0500

    Added a chassis implementation

--- source/domain/robot/chassis.ts
+++ source/domain/robot/chassis.ts
@@ -3,6 +3,7 @@ import { filter } from 'lodash';
 
 export class Chassis {
 	modules: Module[] = [];
+	slots: number;
 
 	getCPUs(): CPU[] {
 		return <any[]>filter(this.modules, module => module.type == ModuleType.cpu);
---
+++ source/domain/robot/chassis/boxChassis.ts
@@ -0,0 +1,8 @@
+import { Chassis } from '../chassis';
+
+export class BoxChassis extends Chassis {
+	constructor() {
+		super();
+		this.slots = 12;
+	}
+}
